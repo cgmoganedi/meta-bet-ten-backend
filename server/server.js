9ca9b443b039b401df703117265c2e1e3ba8330f
chubafusebox <chuba@fusebox.co.za>
2020-03-24 01:53:38 +0200

    first route added

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
-express = require('express');
+const express = require('express');
+const apiRouter = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/chirps', apiRouter);
 
 app.listen(process.env.PORT || '3000', () => {
   console.log(`Server running on port: ${process.env.PORT || '3000'}`);
