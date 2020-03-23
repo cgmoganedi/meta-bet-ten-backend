365a78a4e223749d28dabc5f3e25478877a63694
chubafusebox <chuba@fusebox.co.za>
2020-03-24 01:24:26 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT || '3000', () => {
+  console.log(`Server running on port: ${process.env.PORT || '3000'}`);
+});
