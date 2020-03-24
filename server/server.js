ff468092b3abfbf753f376da61fed668406e1c16
chubafusebox <chuba@fusebox.co.za>
2020-03-24 23:50:50 +0200

    removed test endpoint

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api/chirps', apiRouter);
+app.use('/api', apiRouter);
 
 app.listen(process.env.PORT || '3000', () => {
   console.log(`Server running on port: ${process.env.PORT || '3000'}`);
