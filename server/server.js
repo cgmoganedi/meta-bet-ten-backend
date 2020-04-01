64cb7b98d6f9b7c92627ad5880d661f6c5ae79d0
chubafusebox <chuba@fusebox.co.za>
2020-04-01 08:38:28 +0200

    Added reg and login routes

--- server/server.js
+++ server/server.js
@@ -18,6 +18,68 @@ const users = [];
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'));
+
+app.get('/', checkAuthenticated, (req, res, next) => {
+  res.render('index.ejs', { name: req.user.first_name });
+});
+
+app.get('/login', checkNotAuthenticated, (req, res, next) => {
+  res.render('login.ejs');
+});
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
+
+app.get('/register', checkNotAuthenticated, (req, res, next) => {
+  res.render('register.ejs');
+});
+app.post('/register', checkNotAuthenticated, async (req, res, next) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      star_sign: req.body.star_sign,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login');
+  } catch {
+    res.redirect('/register');
+  }
+});
+
+app.delete('/logout', (req, res) => {
+  req.logOut();
+  res.redirect('/login');
+})
+
+function checkAuthenticated(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/login');
+}
+
+function checkNotAuthenticated(req, res, next){
+  if(req.isAuthenticated()){
+    return res.redirect('/');
+  }
+  next();
+}
 
 app.use('/api', apiRouter);
 
